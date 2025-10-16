8efa2ddd6234472c4c7cd71962f54542c88f3605
Rafew-code <sazzadislam23@gmail.com>
2025-10-16 14:54:58 +0600

    initial terraform vpc with lint gates

---
+++ envs/dev/vpc.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.9.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+resource "aws_vpc" "default" {
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
